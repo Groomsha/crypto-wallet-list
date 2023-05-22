4e20eb9f55b8bbd70cef72f571975ee620322631
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2023-05-22 14:45:30 +0300

    add: settings src

--- gui_app/window_ctk/gui_ctk.py
+++ gui_app/window_ctk/gui_ctk.py
@@ -6,6 +6,7 @@ Ihor Cheberiak (c) 2023
 https://www.linkedin.com/in/ihor-cheberiak/
 """
 
+from typing import Dict
 
 import customtkinter
 
@@ -14,9 +15,11 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 class GUI_ctk(customtkinter.CTk):
-    def __init__(self) -> None:
+    def __init__(self, settings: Dict) -> None:
         super().__init__()
 
         self.app_ctk = self
+        self.settings = settings
+
         self.app_ctk.geometry("400x240")
         self.app_ctk.title("Crypto Wallet List")
--- main.py
+++ main.py
@@ -6,12 +6,32 @@ Ihor Cheberiak (c) 2023
 https://www.linkedin.com/in/ihor-cheberiak/
 """
 
+import json
+from typing import Dict
+
 from gui_app.window_ctk.gui_ctk import GUI_ctk
 
 
 def main() -> None:
-    gui_tk = GUI_ctk()
+    settings = settings_app()
+
+    gui_tk = GUI_ctk(settings)
     gui_tk.mainloop()
+
+
+def settings_app() -> Dict:
+    try:
+        with open("settings.json", "r") as j:
+            settings_temp: Dict = json.load(j)
+        return settings_temp
+    except FileNotFoundError:
+        settings_temp: Dict = {'language': 'EN',
+                               'proxy': 'False'}
+
+        with open("settings.json", "w") as j:
+            json.dump(settings_temp, j)
+
+        return settings_temp
 
 
 if __name__ == "__main__":
