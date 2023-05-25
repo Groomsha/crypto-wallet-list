024064825d48dd41b9d33f156bd63e612c7237e0
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2023-05-25 17:20:33 +0300

    fix: settings lang

--- gui_app/window_ctk/settings_frame.py
+++ gui_app/window_ctk/settings_frame.py
@@ -19,7 +19,8 @@ class SettingsFrame:
         self._window = ctk
         self._lang = lang
 
-        self._image_icon = None
+        self._new_settings: Dict = {}
+        self._image_icon: customtkinter.CTkImage = None
         self._image_settings_frame()
 
         self._settings_frame = customtkinter.CTkFrame(self._window, corner_radius=0, fg_color="transparent")
@@ -38,7 +39,7 @@ class SettingsFrame:
         self._image_icon = customtkinter.CTkImage(Image.open(os.path.join(self._window.image_path, "image_icon_light.png")), size=(20, 20))
 
     def _btn_settings_frame(self) -> None:
-        self._btn_settings_save = customtkinter.CTkButton(self._settings_frame, text=f"{self._lang.language_app.get('btn_settings_save')}", image=self._image_icon, compound="top")
+        self._btn_settings_save = customtkinter.CTkButton(self._settings_frame, text=f"{self._lang.language_app.get('btn_settings_save')}", image=self._image_icon, compound='top', command=self._change_settings_save)
         self._btn_settings_save.grid(row=0, column=3, padx=20, pady=10)
 
         self._btn_settings_mode = customtkinter.CTkOptionMenu(self._settings_frame, values=[f"{self._lang.language_app.get('mode_settings_theme_one')}", f"{self._lang.language_app.get('mode_settings_theme_two')}"], command=self._change_settings_mode_event)
@@ -55,8 +56,17 @@ class SettingsFrame:
         else:
             ref_mode: str = "System"
 
-        self._settings.save({'theme': ref_mode})
+        self._new_settings.update({'theme': ref_mode})
         customtkinter.set_appearance_mode(ref_mode)
 
     def _change_settings_language(self, lang: str) -> None:
-        pass
+        if lang == "English" or lang ==  'Англійська':
+            self._new_settings.update({'language': 'EN'})
+        elif lang == "Ukrainian" or lang == 'Українська':
+            self._new_settings.update({'language': 'UA'})
+        else:
+            self._new_settings.update({'language': 'EN'})
+
+    def _change_settings_save(self) -> None:
+        for key, val in self._new_settings.items():
+            self._settings.save({key: val})
